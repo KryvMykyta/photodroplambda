d64b96b638de7dc61185d05a4e6f1cbda340399f
Kryv Mykyta <kryvmykyta@gmail.com>
2023-04-30 13:35:00 +0300

    made correct watermark resolution

--- utils/PhotoEditor.ts
+++ utils/PhotoEditor.ts
@@ -5,15 +5,15 @@ export class PhotoEditor {
     const resizedPhoto = await sharp(bufferImage).resize(width, height).toBuffer();
     return resizedPhoto;
   };
-
   public addWatermark = async (bufferImage: Buffer, watermark: Buffer) => {
     const image = sharp(bufferImage)
     const metadata = await image.metadata()
-    const watermarkResized = await sharp(watermark).resize(metadata.width, metadata.height).png().toBuffer()
+    const watermarkHeight = parseInt((metadata.height! * 0.41).toFixed(), 10)
+    const watermarkWidth = parseInt((metadata.width! * 0.9).toFixed(), 10)
+    const watermarkResized = await sharp(watermark).resize(watermarkWidth, watermarkHeight, { fit: 'fill'}).png().toBuffer()
     const watermarkPhoto = await sharp(bufferImage)
-      .composite([{ input: watermarkResized, tile: true, }])
+      .composite([{ input: watermarkResized }])
       .toBuffer();
     return watermarkPhoto;
   };
-
 }
